ace6d90b0579b8246c610ef050bee281561e48d4
Dipender-Codes <136907557+Dipender-Codes@users.noreply.github.com>
2025-06-07 13:22:05 +1000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
-  console.log(`Website available at http://localhost:${PORT}`);
 });
